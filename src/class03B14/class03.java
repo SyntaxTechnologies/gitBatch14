a309d012b7183b82218099120ac9fdb87db0d308
moazzamsadiq <moazzam@syntaxtechs.com>
2022-09-27 19:51:43 -0400

    second commit

--- src/class03B14/class03.java
+++ src/class03B14/class03.java
@@ -4,6 +4,7 @@ public class class03 {
 	public static void main(String[] args) {
  
 			System.out.println("hey this is a git class");
+			System.out.println("hey this is a change in my project");
 	}
 
 }
